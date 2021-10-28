4a655fcd4409592c9419a5db73bf356336f30ed2
mikha <mikha@DESKTOP-73HC22V>
2021-10-28 17:41:23 +0500

    Добавьте файлы проекта.

---
+++ lab1_zadanie2_prt-20-2b_belousov/lab1_zadanie2_prt-20-2b_belousov.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double n, n1, n2, m, m1, m2;
+    cout << "Введите переменные n и m:" << endl;
+    cin >> n >> m;
+    n1 = n;
+    n2 = n;
+    m1 = m;
+    m2 = m;
+    cout << "n---m = " << n-- - m << endl;
+    if (n1-- < m1) {
+        cout << "Выражение n--<m верно" << endl;
+    }
+    else { cout << "Выражение n--<m не верно" << endl; }
+    if (n2++ > m2) {
+        cout << "Выражение n++>m верно" << endl;
+    }
+    else { cout << "Выражение n++>m не верно" << endl; }
+    return 0;
+}
+
+
+
